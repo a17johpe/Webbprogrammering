9aa3dbacbb73c120e4097e21402621a0c194ce38
a17johpe <>
2020-03-26 20:17:02 +0100

    Added working page navigation

---
+++ pageNav.js
@@ -0,0 +1,7 @@
+function showPage(id) {
+  let pages = document.getElementsByClassName("page");
+  for (let i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
